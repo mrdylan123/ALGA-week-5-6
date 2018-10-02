80b24eda1846e5f3663d018e208fc787aaa0f427
Dylan Martens <dylanhs123@hotmail.com>
2018-10-02 13:51:04 +0200

    Added project/solution

---
+++ Alga-Week-5-6/Alga-Week-5-6/Program.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alga_Week_5_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();
+        }
+    }
+}
